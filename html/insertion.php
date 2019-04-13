f770cd21ceb068e403d557d2d96ee6c446c555fd
jizel <jizelgm98@gmail.com>
2019-04-13 17:13:00 +0200

    vbhjnk

--- html/insertion.php
+++ html/insertion.php
@@ -8,7 +8,8 @@
            
             $password = $_POST["password"];
 
-            $interet = $_POST['interet'];
+            $interet = $_POST["interet"];
+            $gender= $_POST["gender"]
 
             if($_POST["password"] == $_POST["confirmation"]) {
                 $link = mysql_connect("localhost", "root", "")
@@ -16,7 +17,7 @@
                 //echo 'Connexion réussie';
                 mysql_select_db ("tp1");
                 
-                $qry="insert into user (email, nom, prenom, mpasse, interer) values ('$email', '$nom', '$prenom', '$password', '$interet')";
+                $qry="insert into user (email, nom, prenom, mpasse, interet, gender) values ('$email', '$nom', '$prenom', '$password', '$interet', '$gender')";
                 echo $qry;
                 mysql_query($qry);
 
